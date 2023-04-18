b2aadcc1aa481c1dfd61c1076647ff8ccafdd89c
Deadsmocker <mathieu.nunu@gmail.com>
2023-04-18 16:29:21 +0200

    debut JS, creation des constantes, refonte fermeture modal et debut des erreurs UI

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,17 +11,61 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-const modalclose = document.querySelector(".close");
+
+// ajout des constantes
+const closeBtn = document.querySelector(".close");
+const submitBtn = document.querySelector(".btn-submit");
+const form = document.getElementById("reserve");
+const closBtnRed = document.getElementById("closeBtnRed");
+const confirmationMsg = document.getElementById("confirmationMsg");
+
+// Ajout des constantes erreurs (phase test UI)
+const first = document.getElementById("first");
+const firstError = document.getElementById("firstError");
+
+const last = document.getElementById("last");
+const lastError = document.getElementById("lastError");
+
+const email = document.getElementById("email");
+const emailError = document.getElementById("emailError");
+
+const birthdate = document.getElementById("birthdate");
+const birthdateError = document.getElementById("birthdateError");
+
+const quantity = document.getElementById("quantity");
+const quantityError = document.getElementById("quantityError");
+
+const location = document.getElementById("location");
+const locationError = document.getElementById("locationError");
+
+const conditions = document.getElementById("conditions");
+const conditionsError = document.getElementById("conditionsError");
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-modalclose.addEventListener("click", function(){
-  modalbg.style.display = "none";
-})
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+// fermeture de la modal #1
+closeBtn.addEventListener("click", closeModal);
+function closeModal() {
+  modalbg.style.display = none;
+}
+
+// validation for submit
+closeBtnRed.style.display = "none";
+confirmationMsg.style.display = "none";
+
+let formOk = true;
+
+
+
+
+
+
 
 
